072888bdba1e1b7540e5d630f842f9bce1c2f14f
Eduard Ramon Maldonado <eduard.ramon.maldonado@gmail.com>
2013-06-26 16:28:13 +0200

    Initial Commit

---
+++ FourierDescriptors/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  FourierDescriptors
+//
+//  Created by Edu on 26/06/13.
+//  Copyright (c) 2013 Itoxi&Edu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
